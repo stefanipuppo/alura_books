9fce6e72d6e537f312a298930c3eb15674a31927
Stefani Dal Puppo <stefanipuppo@hotmail.com>
2024-04-09 14:11:14 -0300

    filter

--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -2,6 +2,7 @@ const elementoParaInserirLivros = document.getElementById('livros')
 
 
 function exibirOsLivrosNaTela(listaDeLivros) {
+    elementoParaInserirLivros.innerHTML = ''
     listaDeLivros.forEach(livro => {
         elementoParaInserirLivros.innerHTML += `
         <div class="livro">
